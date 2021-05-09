473dcdcbc92dcd8572329abb52e9f22513892746
bart capote <gauclee@gmail.com>
2021-05-09 14:35:28 +0200

    Add mock setup test

---
+++ src/Codecool.BookDb.Tests/MockTests.cs
@@ -0,0 +1,29 @@
+﻿using Codecool.BookDb.Model;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Codecool.BookDb.Tests
+{
+[TestFixture]
+    class MockTests
+    {
+        [Test]
+        public void MockSetupTest()
+        {
+            var mockAuthorDao = new Mock<IAuthorDao>();
+            var author = new Author("bart", "capote", DateTime.Now);
+
+            mockAuthorDao.Setup(x => x.Get(1)).Returns(author);
+
+            var returnedAuthor = mockAuthorDao.Object.Get(1);
+
+            mockAuthorDao.Object.Add(author);
+            mockAuthorDao.Verify(x => x.Add(It.IsAny<Author>()), Times.Once);
+
+            Assert.That(returnedAuthor, Is.EqualTo(author));
+        }
+
+
+    }
+}
